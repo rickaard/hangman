81dea53aedb9cb167938e27f78c669e418486867
Rickard <rickaard@gmail.com>
2021-08-22 11:53:36 +0200

    removed some debug logging

--- lib/hangman_web/live/enter_game_live.ex
+++ lib/hangman_web/live/enter_game_live.ex
@@ -9,15 +9,11 @@ defmodule HangmanWeb.Enter_GameLive do
   end
 
   def handle_event("enter_room", %{"user" => %{"name" => name, "room_id" => room_id}} = _params, socket) do
-    # IO.inspect params, label: "** params | enter-room **"
-    # IO.inspect socket, label: "** params | enter-room **"
     socket = assign(socket, name: name, room_id: room_id)
     {:noreply, redirect(socket, to: "/game/#{room_id}?name=#{name}")}
-    # {:noreply, socket}
   end
 
   def handle_event("validate", %{"user" => user_params} = _user_params, socket) do
-    # IO.inspect user_params, label: "validate params"
     changeset = %User{}
     |> User.changeset(user_params)
     |> Map.put(:action, :insert)
